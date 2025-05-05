26f31f917909b8854546a4fb75eb3ed509867640
vishalvky007 <9512278k@gmail.com>
2025-05-06 04:06:28 +0530

    Flask port to 8000

--- config.py
+++ config.py
@@ -19,4 +19,4 @@ MODEL_NAME = 'gemini-1.5-flash-latest'
 
 # Flask Configuration
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
-PORT = int(os.getenv('PORT', 5000)) 
+PORT = int(os.getenv('PORT', 8000)) 
